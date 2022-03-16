bbc3908113faf69adfc0cef64b5adc05ec4baad2
istefanoff <official.stefanov@gmail.com>
2022-03-16 14:46:57 +0200

    Added InstrumentsDAO and TeachersDAO

--- src/main/java/org/yonitutu/music_academy/data/dao/impl/BaseDao.java
+++ src/main/java/org/yonitutu/music_academy/data/dao/impl/BaseDao.java
@@ -1,5 +1,7 @@
 package org.yonitutu.music_academy.data.dao.impl;
 
+import org.yonitutu.music_academy.data.dao.api.functions.ExecuteTransactional;
+
 import javax.persistence.EntityManager;
 
 public abstract class BaseDao {
@@ -7,5 +9,15 @@ public abstract class BaseDao {
 
     protected BaseDao(EntityManager entityManager) {
         this.entityManager = entityManager;
+    }
+
+    void executeTransactional(ExecuteTransactional ex){
+        this.entityManager.getTransaction().begin();
+
+        ex.execute();
+
+        this.entityManager.flush();
+
+        this.entityManager.getTransaction().commit();
     }
 }
